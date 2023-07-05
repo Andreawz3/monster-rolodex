b50c150a2174040b8185576dd1a60f95a417f386
Andrea Wu <andrea.wzg@gmail.com>
2023-07-05 15:08:36 -0700

    add an 'input-search' box with filter and edit the README.md file content

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  // This run first
   constructor() {
     super();
 
@@ -11,7 +12,7 @@ class App extends Component {
       monsters: []
     };
   }
-
+  // This run third
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
@@ -23,9 +24,25 @@ class App extends Component {
     }
     ));
   }
- 
+  // This run second
   render() {
     return <div className="App">
+      <input 
+        className='search-box' 
+        type='search' 
+        placeholder='search monsters' 
+        onChange={(event) => {
+          console.log(event.target.value);
+          const searchString = event.target.value.toLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLowerCase().includes(searchString);
+          });
+
+          this.setState(() => {
+            return { monsters : filteredMonsters}
+          })
+        }}
+      />
       {this.state.monsters.map((monster) => {
         return (
           <div key={monster.id}>
